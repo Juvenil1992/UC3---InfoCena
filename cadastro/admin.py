ec9af3c15bd6f77b9ec9a86bd0cd35ca5cb30ff2
Informática Fundamental 2024.5 <39128522024.5@senacrjedu.local>
2025-04-28 21:31:43 -0300

    voltando ao InfoCena

---
+++ cadastro/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+
+from cadastro.models import Loja
+
+# Register your models here.
+
+admin.site.register(Loja)
---
+++ cadastro/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Loja(models.Model):
+    nome = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20)
+
